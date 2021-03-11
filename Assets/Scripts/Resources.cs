e1a5b43be24a5f618043965b621ba89940ba2103
MartsLV <39812038+MartsLV@users.noreply.github.com>
2021-03-12 00:33:36 +0200

    Updates

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Resources : MonoBehaviour
 {
-    public float resource_Wood = 0;
+    public int resource_Wood = 0;
+    public int resource_Stone = 0;
+    public int resource_Metal = 0;
     Rigidbody rbcomponent;
     private Camera mainCamera;
+    [SerializeField] public int tool;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +27,22 @@ public class Resources : MonoBehaviour
 
         if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition),out hit))
         {
-            if(hit.collider.tag == "Wood"&&Input.GetMouseButtonDown(0))
+            if(hit.collider.tag == "Wood"&&Input.GetMouseButtonDown(0)&&tool==1)
             {
                 resource_Wood += 1;
                 Debug.Log(resource_Wood);
             }
-            
+            if (hit.collider.tag == "Stone" && Input.GetMouseButtonDown(0) && tool == 2)
+            {
+                resource_Stone += 1;
+                Debug.Log(resource_Stone);
+            }
+            if (hit.collider.tag == "Metal" && Input.GetMouseButtonDown(0) && tool == 3)
+            {
+                resource_Metal += 1;
+                Debug.Log(resource_Metal);
+            }
+
 
 
         }
