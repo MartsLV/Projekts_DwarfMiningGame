a63852b6f8d91f14e38547dc455936f3595c61c6
MartsLV <39812038+MartsLV@users.noreply.github.com>
2021-03-11 17:16:18 +0200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    
+    
+    public Vector3 pointToLook;
+
+    private float X_axis;
+    private float Z_axis;
+    float Angle;
+    Rigidbody rbcomponent;
+    [SerializeField] Transform target;
+    
+    private Camera mainCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbcomponent = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        X_axis = Input.GetAxis("Horizontal");
+        Z_axis = Input.GetAxis("Vertical");
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            pointToLook = cameraRay.GetPoint(rayLength);
+            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rbcomponent.velocity = new Vector3(X_axis, rbcomponent.velocity.y, Z_axis);
+        transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+
+    }
+    
+}
---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : MonoBehaviour
+{
+    public float resource_Wood = 0;
+    Rigidbody rbcomponent;
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbcomponent = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition),out hit))
+        {
+            if(hit.collider.tag == "Wood"&&Input.GetMouseButtonDown(0))
+            {
+                resource_Wood += 1;
+                Debug.Log(resource_Wood);
+            }
+            
+
+
+        }
+            
+                
+    }
+}
